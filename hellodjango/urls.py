5eaae5d9a54a7a2168f7d9f50f2fef855a28f9eb
sandeep <schourey1988@gmail.com>
2015-04-17 16:01:47 +0530

    hellodjango/urls.py

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hellodjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-] 
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
